4d9dfc94e34215613fd69b385d12f43ae7277d5e
eduanlima <eduandev@gmail.com>
2024-04-15 06:36:04 -0300

    Product entity SQL script

--- backend/src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO category (name, createdat) VALUES ('Livros', NOW());
-INSERT INTO category (name, createdat) VALUES ('Eletrônicos', NOW());
-INSERT INTO category (name, createdat) VALUES ('Computadores', NOW());
-
-
